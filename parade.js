6c1181d1355d759de3d34cf1c327bfdf74c4c800
martindaniel4 <martindaniel@192.168.0.21>
2014-04-23 16:08:47 +0200

    init

--- parade.js
+++ parade.js
@@ -1,135 +1,15 @@
 if (Meteor.isClient) {
 
-var paradeData =  [
-  {
-    "value":"http://www.youtube.com/watch?v=kKv127gtA70",
-    "date":"28/11/2013"
-  },
-  {
-    "value":"http://fr.wikihow.com/nettoyer-une-%C3%A9ponge",
-    "date":"29/11/2013"
-  },
-  {
-    "value":"L'architecture c'est, avec des matériaux bruts, établir des rapports émouvants",
-    "date":"30/11/2013"
-  },
-  {
-    "value":"Marcel Duchamp et Yves Tanguy prirent un jour un peu de cocaine pour se donner le courage d'aller, pour la première fois voir André Breton qui ne comprit rien à leur discours.",
-    "date":"01/12/2013"
-  },
-  {
-    "value":"This world is wild at heart and weird on top",
-    "date":"02/12/2013"
-  },
-  {
-    "value":"Bourriaud est un con.",
-    "date":"03/12/2013"
-  },
-  {
-    "value":"http://www.youtube.com/watch?v=z-Lhu4JFePk",
-    "date":"04/12/2013"
-  },
-  {
-    "value":"http://bit.ly/ImkE9k",
-    "date":"05/12/2013"
-  },
-  {
-    "value":"http://bit.ly/1iQBpaj",
-    "date":"06/12/2013"
-  },
-  {
-    "value":"FLCG. Front de Libération du Cheddar Grillé. ",
-    "date":"07/12/2013"
-  },
-  {
-    "value":" ..et c etait comme un concentre de mots justes qui font du bien",
-    "date":"08/12/2013"
-  },
-  {
-    "value":"http://bit.ly/1c7umXR",
-    "date":"09/12/2013"
-  },
-  {
-    "value":"amour\nbella\naime\nenvie\nbelle\ndormir\nbabe\nbaby",
-    "date":"10/12/2013"
-  },
-  {
-    "value":"Note que..",
-    "date":"11/12/2013"
-  },
-  {
-    "value":"Doucement, y a mes parents juste à côté",
-    "date":"12/12/2013"
-  },
-  {
-    "value":"l'histoire fascinante de la moon cup",
-    "date":"13/12/2013"
-  },
-  {
-    "value":"Ah non, ça c'est ton casque",
-    "date":"14/12/2013"
-  },
-  {
-    "value":"Di u vi salve regina\nEe madre univerzale\nPerqui favore zizare\nAl paradizu (bis)\n\nVo oi siete joya i risu\nDi i tutti scunzolati\nDi tutti tribulati\nLunica speme (bis)\n\n",
-    "date":"15/12/2013"
-  },
-  {
-    "value":"Il n'y a pas que la moustache qui m'excite",
-    "date":"16/12/2013"
-  },
-  {
-    "value":"http://www.youtube.com/watch?v=wHxnvxclme0",
-    "date":"17/12/2013"
-  },
-  {
-    "value":"Sometimes there's a man… I won't say hero, 'cause what's a hero? but sometimes there's a man, and I'm talking about the Dude here, sometimes there's a man… ",
-    "date":"18/12/2013"
-  },
-  {
-    "value":"http://bit.ly/1eyl8nv",
-    "date":"19/12/2013"
-  },
-  {
-    "value":"10 novembre, 20h30 et deux bafles à descendre",
-    "date":"20/12/2013"
-  },
-  {
-    "value":"http://www.cndp.fr/uploads/pics/08_02.jpg",
-    "date":"21/12/2013"
-  },
-  {
-    "value":"Je m'ennuie de toi",
-    "date":"22/12/2013"
-  },
-  {
-    "value":"http://bit.ly/1b3WMgN",
-    "date":"23/12/2013"
-  }
-]
-var format = d3.time.format("%d/%m/%Y");
-var today = new Date();
+var paradeData =  ["","What are you going to do?","What are the obstacles in your path?","What are people forced to do now that your product does not exist yet?","What are the barriers of entry you'll have to break down to enter this market?","Who needs what you are making?","What exactly is it that you are building?","How do you know your users need what you are building?","How will your users find you?","Who needs what you're making?","How do you know your users need it? What are your users doing now that you don't exist yet?","What, exactly, makes you different from existing options?","Why isn't someone already doing this?","What obstacles will you face and how will you overcome them?","How will customers and/or users find out about you?","What resistance will users have to trying you?","How will you overcome your users' resistance?","What are the key things about your field that outsiders don't understand?","What part of your project are you going to build first?","Who is going to be your first paying customer?","If your startup succeeds, what additional areas might you be able to expand into?","Why did you choose this idea?","What have you learned so far from working on your idea?","Six months from now, what's going to be your biggest problem?","Where do new users come from?","What is your traffic trend like?","What's the conversion rate?","What makes new users try you?","Why do the reluctant users hold back?","What are the top things users want?","What has surprised you about user behavior?","What is it that you do? What exactly are you building? What are you going to do?","How do you know the user want it?","Who are your users?","Who are your target users?","How are you going to scale?","What’s unique about you?","What are the insights you got from your users?","What usage from your users surprised you?","What's the magic?","What's the barrier to entry?","How do you make money?","What's the benefit for the user?","Which subset of your users is most desperately waiting for the solution you are building?","What has surprised you about user behavior?","Who is going to start using you the soonest? 3 months from now? 6 months from now?","How do you know your users need what you are building?","Why did you pick this specific idea? What are your motivations?","Why are you the right team to do this?","Why is the world better because what you built exists!","What’s your background in this problem space?","Where’s the rocket science in this?","What are people forced to do now that your product does not exist yet?","How do you know that you are building the right product?","What are the top things users want?","Why doesn’t it already exist?","What part of your project are you going to build first? What are you going to build next?","What’s the biggest missing feature? The one thing customers keep asking for?","How do you know users have the problem you're solving?","How will you know when your product goes viral?","Which features are you launching with? Which ones will you build later?","Are you going to internationalize?","What keeps you awake at night","How are you going to explain the app to first-time users?","If you had to delete one feature, which one would be it?","Why doesn’t it already exist?","Why will people want this?","Which subset of your users is most desperately waiting for the solution you are building?","How do we know users have the problem we're solving?","What do your users say about your product","What are the top things users want?","Why do the reluctant users hold back?","How much time to visitors spend on your site / app / product?","What are the top things users want?","How many people downloaded / installed / used your product?","What's the biggest driver of traffic?","What actually does a typical user do with your product?","What is your traffic trend like? What’s your growth rate like?","What's the conversion rate?","How committed are you?","What are your problems? What is screwed up? What is going wrong?","What can you do in person to make sure the product will be successful?","Is this something you want to spend your next 5 years working on? Why?","Are you open to changing your idea?","Is this the biggest problem you could find?","Why aren't your engineers working on something bigger instead of this?","What are your technical costs?","Why is what you are building technically demanding?","What are the keywords people are going to type into Google to find you?","How are you going to solve the chicken & egg problem?","Where does your traffic come from? How will people find you?","What are you going to do to increase the virality of your product?","What resistance will users have to trying you?","How big do you think you can get?","What are the criteria you will base your decision to launch on?","How are you going to scale this to become a massive company?","What are the obstacles in your path?","Why are people quitting your app? Why are people giving up?","Six months from now, what's going to be your biggest problem?","What do you do to encourage your people to keep using it?","What is going to make this thing stay in the top of people’s heads?","How are you going to scale this?","What will make your product stand out in the marketplace?","Is this something that people are going to want to do over and over again?","How hard is it to convince people to use your product?","Where are your customers? How will they find you?","How are you going to sell it to your customers?","What’s the customer lifetime value?","What’s the customer acquisition cost?","Have you asked people to pay real money?","How you will sell it to customers?","Who is going to be your first paying customer?","If your startup succeeds, what additional areas might you be able to expand into?","What’s the customer lifetime value?","What’s the customer acquisition cost?","How are youg going to scale this?","How much are you going to charge your users?","What are you charging money for?","How much are your users spending on similar solutions to yours?","Have you asked people to pay real money?","Which specific things are you doing better than your competition?","What things does your app have that competitors don’t?","What are you offering that no one else can replicate?","What is you app, service or website NOT like?","What is the size of your market?","How are you going to break down the barriers of entry?","What are the key things about your field that outsiders don't understand?","What's the revenue of your closest competitor? What is his traffic?","Why isn't someone already doing this?","What are people using now instead of your solution?","How are you going to make your users switch from competing products to yours?","Why are you going to succeed?"];
+
 
   Template.hello.greeting = function () {
     
-   var paradeNest = d3.nest().key(function(d) {return d.date;}).map(paradeData);
+    var length = paradeData.length;
+    
+    var random = Math.round(Math.random()*length);
 
-    if (paradeNest[format(today)] != undefined) {
-
-    return paradeNest[format(today)][0].value;
-
-      } else {
-
-        return "désolé petit baby, la parade n'a pas encore commencé.";
-
-      }
-
-  };
-
-  Template.hello.date = function() {
-
-   var diffDate = Math.round((format.parse("01/01/2014")-today)/(1000*60*24*60));
-
-    return " - " + diffDate + " jours";
+    return paradeData[random];
 
   };
 
